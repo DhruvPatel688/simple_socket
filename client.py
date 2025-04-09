67285c07c83b7007305dc712edd0c803d32e52c4
Dhruv Patel <dhruvtn04@gmail.com>
2025-04-09 10:59:06 -0500

    Exit has been fixed and all ids should be shown

--- client.py
+++ client.py
@@ -6,8 +6,11 @@ import threading
 
 def receive(c):
     while True:
-        message = c.recv(1024).decode('utf-8')
-        print(message)
+        try:
+            message = c.recv(1024).decode('utf-8')
+            print(message)
+        except Exception as e:
+            break
 
 
 #use a conditional in the loop to exit.
@@ -16,19 +19,24 @@ def client():
     #create a client socket to send and receive messages.
     c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = '127.0.0.1'
-    port = 12345
+    port = 8000
     #assign client the proper ip address and port number
     c.connect((host, port))
     
     client_handler = threading.Thread(target=receive, args=(c,))
+    client_handler.daemon = True
     client_handler.start()
     #need a loop to receive messages and process the data better
     #to continue client and server interaction until exit
     while True:
         data = input('Enter your message: ')
-
+        if data == '.exit':
+            print('Exiting')
+            c.close()
+            break
+        else:
         #give the client the ability to send a message to the server
-        c.sendall(bytes(data,'utf-8'))
+            c.sendall(bytes(data,'utf-8'))
 
         #include bonus features such as receiving message receipts 
 
--- server.py
+++ server.py
@@ -5,8 +5,8 @@ import threading
 import os
 import uuid
 
-list_of_clients = []
-
+#list_of_clients = []
+list_of_clients = {}
 
 
 #Add more information inside this loop to give message receipts and broadcast clients connected to server 
@@ -14,16 +14,21 @@ def accept_clients(c, addr):
     while True:
         
         data = c.recv(1024).decode('utf-8')
+        if not data:
+            message = addr + ' has disconnected.'
+            broadcast(bytes(message,'utf-8'), c)
+            break
+
         print("Received Message: ", data, " from", addr)
         #Still need to add 'exit' conditional for client to exit server.
-
-        receipt = 'Received message ' + data + ' from '+ addr
+        
+        receipt = '\nReceived message \'' + data + '\' from '+ addr
         broadcast(bytes(receipt,'utf-8'), c)
     c.close()
 
 #used to broadcast message to all clients
 def broadcast(message, connection): 
-    for clients in list_of_clients: 
+    for clients, value in list(list_of_clients.items()): 
         if clients!=connection: 
             try: 
                 clients.send(message) 
@@ -31,24 +36,29 @@ def broadcast(message, connection):
                 clients.close() 
  
                 # if the link is broken, we remove the client 
-                remove(clients) 
+                del list_of_clients[clients]
 
 #this is the main server function that connects to clients
 def server():
     #create a socket on the server side to recieve message
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+    
     host = '127.0.0.1'
-    port = 12345
+    port = 8000
     s.bind((host, port))
     s.listen(5)
     print(f"Server listening on {host}:{port}")
     #Accept the connection and print in terminal which address server is connected with until client disconnects
+    
     while True:
         c, addr = s.accept()
         #still need to add unique ID:
         unique_id = str(uuid.uuid4())
-        list_of_clients.append(c)
+        #list_of_clients.append(c)
+        list_of_clients[c] = unique_id
+        for key,value in list_of_clients.items():
+            message = f"\nOther client connected is {value}"
+            broadcast(bytes(message,'utf-8'), key)
         #for client in list_of_clients:
         #    for c in list_of_clients:
         #        if client != c:
